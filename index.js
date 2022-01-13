51eedf7748c0d3cae15bf7d1eeef450dba6e2038
Gs-Dip <dibbo12345678ghosh@gmail.com>
2022-01-13 14:55:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// java script 89 no video(chack java script note book)
+
+
+/**
+* addNumbers function will add 2 numbers 
+*@param {number} x -first number
+*@param {number} y -second number
+*/
+
+
+function addNumbers(){
+    console.log(x+y)
+}
+
+
+addNumbers() //akhane mouse rakhar sathe sathe opore comment ar moddhe ja save korechi ta hover kore dekhabe.....aita akta coding standard podhoti.... 
